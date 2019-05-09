156bee6357dfd0ee7eb953fbadc700afd4dbae96
ajanyaparus <43316630+ajanyaparus@users.noreply.github.com>
2019-05-09 13:41:54 +0530

    4th sprint

--- database.sql
+++ database.sql
@@ -27,11 +27,11 @@ CREATE TABLE `admin_report` (
   `status` varchar(29) DEFAULT NULL,
   `department` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 /*Data for the table `admin_report` */
 
-insert  into `admin_report`(`id`,`title`,`report`,`date`,`status`,`department`) values (1,'aahkdd','jkshdddddddjdkf','2019-04-02','pending','cyber cell'),(2,'aahkdd','jkshdddddddjdkf','2019-04-02','pending','cyber cell'),(3,'fjgjkdjjfg','ghgkkkkkkkkkkkkkkkk','2019-04-02','pending','human rights'),(4,'affhh','njnkjmk','2019-04-02','pending','human rights');
+insert  into `admin_report`(`id`,`title`,`report`,`date`,`status`,`department`) values (1,'aahkdd','jkshdddddddjdkf','2019-04-02','qqq','cyber cell'),(2,'aahkdd','jkshdddddddjdkf','2019-04-02','replied','cyber cell'),(3,'fjgjkdjjfg','ghgkkkkkkkkkkkkkkkk','2019-04-02','pending','human rights'),(4,'affhh','njnkjmk','2019-04-02','pending','human rights'),(5,'fgf','img1475915812154.jpg','2019-04-20','pending','Human Right'),(6,'vnbbc','Pink_police_new.docx','2019-04-20','pending','Cyber Cell'),(7,'fgd','smrithi.docx','2019-04-20','replied','Human Right');
 
 /*Table structure for table `collect_tweet` */
 
@@ -47,21 +47,70 @@ CREATE TABLE `collect_tweet` (
 
 /*Data for the table `collect_tweet` */
 
+/*Table structure for table `law` */
+
+DROP TABLE IF EXISTS `law`;
+
+CREATE TABLE `law` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cid` int(11) DEFAULT NULL,
+  `description` varchar(54) DEFAULT NULL,
+  `file` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+/*Data for the table `law` */
+
+insert  into `law`(`id`,`cid`,`description`,`file`) values (1,3,'sdfsfsd','domestic.pdf'),(2,3,'ghdg','log.docx'),(6,7,'dguf','EXISTING_SYSTEM_indoor.docx');
+
 /*Table structure for table `login` */
 
 DROP TABLE IF EXISTS `login`;
 
 CREATE TABLE `login` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `userid` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `type` varchar(20) NOT NULL,
-  PRIMARY KEY (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`,`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 /*Data for the table `login` */
 
-insert  into `login`(`id`,`userid`,`password`,`type`) values (2,'admin','admin','admin'),(3,'cc','111','cyber cell'),(1,'hr','123','Human Right');
+insert  into `login`(`id`,`userid`,`password`,`type`) values (1,'hr','123','Human Right'),(2,'admin','admin','admin'),(3,'cc','abcdefg','Cyber Cell'),(6,'\"+userid+\"','\"+psd+\"','\"+department+\"'),(12,'kiio','123456','Human Right'),(14,'kiio','7777','Human Right'),(15,'hty@gmail.com','9078451234','public'),(16,'anu@gmail.com','123','Cyber Cell'),(17,'manu@gmail.com','890890','public'),(18,'ammu@gmail.com','123456','Human Right'),(19,'arya@gmail.com','9072008899','public');
+
+/*Table structure for table `post` */
+
+DROP TABLE IF EXISTS `post`;
+
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) DEFAULT NULL,
+  `post` varchar(45) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `post` */
+
+insert  into `post`(`id`,`uid`,`post`,`date`) values (1,15,'hhhhhhhhhhhhhhhhhh','2019-04-16'),(2,12,'jhjgjhghj','2019-04-20');
+
+/*Table structure for table `publicregistration` */
+
+DROP TABLE IF EXISTS `publicregistration`;
+
+CREATE TABLE `publicregistration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(54) DEFAULT NULL,
+  `place` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+
+/*Data for the table `publicregistration` */
+
+insert  into `publicregistration`(`id`,`name`,`place`,`email`,`phone`) values (15,'sonu','Calicut','hty@gmail.com','9078451234'),(17,'manu','clt','manu@gmail.com','890890'),(19,'arya','calicut','arya@gmail.com','9072008899');
 
 /*Table structure for table `reg` */
 
@@ -85,7 +134,7 @@ CREATE TABLE `reg` (
 
 /*Data for the table `reg` */
 
-insert  into `reg`(`uid`,`userid`,`department`,`street`,`city`,`post`,`pin`,`district`,`email`,`phone`,`fax`,`chairperson`) values (1,'aj@gmail.com','Human Right','hh','hh','hh','67688','hh','aj@gmail.com',67878978,3647689,'aj'),(2,'hr@gmail.com','Human Right','pookkad','pookkad','pookkad','67688','calicut','hr@gmail.com',2222,222000,'ajanya s');
+insert  into `reg`(`uid`,`userid`,`department`,`street`,`city`,`post`,`pin`,`district`,`email`,`phone`,`fax`,`chairperson`) values (0,'\" + userid + \"','\" + department + \"','\" + street + \"','\" + city + \"','\" + post + \"','\" + pin + \"','\" + district + \"','\" + email + \"',0,0,'\" + chairperson + \"'),(1,'aj@gmail.com','Human Right','hh','hh','hh','67688','hh','aj@gmail.com',67878978,3647689,'aj'),(18,'ammu@gmail.com','Human Right','jkjh','jfgh','hjh','678998','kozhikode','ammu@gmail.com',9078123456,0,'khk'),(16,'anu@gmail.com','Cyber Cell','calicut','calicut','kadalundi','673314','calicut','anu@gmail.com',9078123456,0,'jadksad'),(2,'hr@gmail.com','Human Right','pookkad','pookkad','pookkad','67688','calicut','hr@gmail.com',2222,222000,'ajanya s'),(14,'kiio','Human Right','uuuu','uuuu','788788','998877','jjjj','kiio',8888768788,0,'mk');
 
 /*Table structure for table `report_reply` */
 
@@ -102,7 +151,38 @@ CREATE TABLE `report_reply` (
 
 /*Data for the table `report_reply` */
 
-insert  into `report_reply`(`id`,`report_id`,`title`,`file`,`date`) values ('1',0,'report','domestic.pdf','2019-03-26'),('2',3,'log','log.docx','2019-03-28');
+insert  into `report_reply`(`id`,`report_id`,`title`,`file`,`date`) values ('1',2,'report','domestic.pdf','2019-03-26'),('2',3,'log','log.docx','2019-03-28');
+
+/*Table structure for table `reportreply` */
+
+DROP TABLE IF EXISTS `reportreply`;
+
+CREATE TABLE `reportreply` (
+  `rid` int(11) NOT NULL AUTO_INCREMENT,
+  `report_id` int(11) DEFAULT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `file` varchar(50) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`rid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `reportreply` */
+
+insert  into `reportreply`(`rid`,`report_id`,`title`,`file`,`date`) values (1,2,'jjj','tble_example.docx','2019-04-20'),(2,7,'mmm','Doc2.docx','2019-04-20');
+
+/*Table structure for table `view_tweets` */
+
+DROP TABLE IF EXISTS `view_tweets`;
+
+CREATE TABLE `view_tweets` (
+  `id` int(10) unsigned NOT NULL,
+  `tweet` varchar(500) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `view_tweets` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
--- domestic.py
+++ domestic.py
@@ -1,13 +1,10 @@
+
 from  flask import *
-import os
-from werkzeug.utils import secure_filename
 import MySQLdb
 con=MySQLdb.connect(host='localhost',user='root',passwd='root',port=3306,db='dvcs')
 cmd=con.cursor()
 root=Flask(__name__)
-root.secret_key='abc'
-UPLOAD_FOLDER = './static/file'
-root.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @root.route('/')
 def main():
     return render_template("login.html")
@@ -17,27 +14,16 @@ def login():
     passd=request.form['pass']
     cmd.execute("select * from login where userid='"+user_name+"' and password='"+passd+"'")
     s=cmd.fetchone()
-
-
     if s is None:
         return '''<script>alert('invalid ...');window.location='/'</script>'''
     elif s[3]=='admin':
-        session['id'] = s[0]
-        return '''<script>alert('scucessfull ...');window.location='/home'</script>'''
-    elif s[3]=='Cyber Cell':
-        session['id'] = s[0]
-        return '''<script>alert('scucessfull ...');window.location='/cyberhome'</script>'''
+        return render_template("adminhome.html")
+    elif s[3]=='cyber cell':
         return render_template("cybercell home.html")
-    elif s[3] == 'Human Right':
-        session['id'] = s[0]
-        return '''<script>alert('scucessfull ...');window.location='/hhome'</script>'''
-
     else:
-        return render_template("publichome.html")
+        return render_template("human right home.html")
 
-@root.route('/pubhome')
-def pubhome():
-    return render_template("publichome.html")
+
 
 
 
@@ -45,57 +31,9 @@ def pubhome():
 def home():
     return render_template("adminhome.html")
 
-@root.route('/cyberhome')
-def cyberhome():
-    return render_template("cybercell home.html")
-
-@root.route('/hhome')
-def hhome():
-    return render_template("human right home.html")
-
 @root.route('/regdep')
 def regdep():
     return render_template("reg.html")
-
-
-@root.route('/uplaws')
-def uplaws():
-    return render_template("uploadlaw.html")
-@root.route('/viewpost')
-def viewpost():
-    cmd.execute("select post.*,publicregistration.name from publicregistration  inner join post on post.uid=publicregistration.id")
-    c = cmd.fetchall()
-    print(c)
-    return render_template("viewpost.html", val=c)
-
-@root.route('/uploadlaw',methods=['get','post'])
-def uploadlaw():
-    try:
-        title = request.form['title']
-        file = request.files['file']
-        img = secure_filename(file.filename)
-        id = session['rid']
-        file.save(os.path.join(root.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
-
-        cmd.execute("insert into law values(null,'" + str(id) + "', '" + title + "','" + img + "')")
-        con.commit()
-        return render_template("cybercell home.html")
-    except  Exception as e:
-        print (e)
-
-
-@root.route('/public')
-def public():
-    return render_template("publicreg.html")
-
-@root.route('/publicpost')
-def publicpost():
-    return render_template("post.html")
-
-@root.route('/report_dep')
-def report_dep():
-    return render_template("report dep.html")
-
 @root.route('/regdep2',methods=['get','post'])
 def regdep2():
     userid = request.form['email']
@@ -110,10 +48,9 @@ def regdep2():
     phone = request.form['phn']
     fax = request.form['fax']
     chairperson = request.form['cp']
-    cmd.execute("insert into login values(null,'"+userid+"','"+psd+"','"+department+"')")
-    id=con.insert_id()
-    print(id)
-    cmd.execute("insert into reg values('"+str(id)+"','" + userid + "','" + department + "','" + street + "','" + city + "','" + post + "','" + pin + "','" + district + "','" + email + "','" + phone + "','" + fax + "','" + chairperson + "')")
+    cmd.execute(
+    "insert into reg values('" + userid + "','" + department + "','" + street + "','" + city + "','" + post + "','" + pin + "','" + district + "','" + email + "','" + phone + "','" + fax + "','" + chairperson + "')")
+    cmd.execute("insert into login values('"+userid+"','"+psd+"','"+department+"')")
     con.commit()
     return '''<script>alert("inserted");window.location="regdep"</script>'''
 
@@ -126,10 +63,6 @@ def collect():
 @root.route('/download_report')
 def download_report():
     return render_template("downloadreport.html")
-
-@root.route('/logout')
-def logout():
-    return render_template("login.html")
 
 @root.route('/search_history')
 def search_history():
@@ -144,162 +77,59 @@ def dep_view():
     type=request.form['select']
     print(type)
     if type=='cyber cell':
-        cmd.execute("select reportreply.*,admin_report.department from reportreply join admin_report on reportreply.report_id=admin_report.id where department='"+type+"'")
+        cmd.execute("select report_reply.*,admin_report.department from report_reply join admin_report on report_reply.report_id=admin_report.id where department='"+type+"'")
         c = cmd.fetchall()
-        print(c)
         return render_template("downloadreport.html",val=c)
     else:
-        cmd.execute("select reportreply.*,admin_report.department from reportreply join admin_report on reportreply.report_id=admin_report.id where department='"+type+"'")
+        cmd.execute("select report_reply.*,admin_report.department from report_reply join admin_report on report_reply.report_id=admin_report.id where department='"+type+"'")
         d = cmd.fetchall()
-        print(d)
         return render_template("downloadreport.html",val=d)
 
 @root.route('/send_report')
 def send_report():
-    id = request.args.get('id')
-    session['rid'] = id
     return render_template("sendreport.html")
 @root.route('/send_report2',methods=['get','post'])
 def send_report2():
     title = request.form['title']
-    file = request.files['file']
-    img = secure_filename(file.filename)
-    id=session['rid']
-    file.save(os.path.join(root.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
-
-    cmd.execute("insert into reportreply values(null,'"+str(id)+"', + '"+title + "','"+img+"',curdate())")
-    cmd.execute("update admin_report set status='replied' where id='" + str(id) + "'")
+    file = request.form['file']
+    cmd.execute("insert into report_reply values(null,'"++"''" + title + "','"+file+"',curdate())")
     con.commit()
-    return render_template("cybercell home.html")
-
-@root.route('/human_report')
-def human_report():
-    id = request.args.get('id')
-    session['rid'] = id
-    return render_template("human_sendreport.html")
-@root.route('/human_report2',methods=['get','post'])
-def human_report2():
-    title = request.form['title']
-    file = request.files['file']
-    img = secure_filename(file.filename)
-    id=session['rid']
-    file.save(os.path.join(root.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
-
-    cmd.execute("insert into reportreply values(null,'"+str(id)+"', + '"+title + "','"+img+"',curdate())")
-    cmd.execute("update admin_report set status='replied' where id='" + str(id) + "'")
-    con.commit()
-    return render_template("human right home.html")
-
-@root.route('/report_dept',methods=['get','post'])
-def report_dept():
-    dept=request.form['select']
-    title = request.form['title']
-    file = request.files['file']
-    img=secure_filename(file.filename)
-    file.save(os.path.join(root.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
-
-    cmd.execute("insert into admin_report values(null,'"+title+"','" + str(img) + "',curdate(),'pending','"+dept+"')")
-    con.commit()
-    return render_template("adminhome.html")
+    return render_template("sendreport.html")
 @root.route('/change_psd')
 def change_psd():
     return render_template("change password.html")
 @root.route('/change_psd2',methods=['get','post'])
 def change_psd2():
-        user_id = session['id']
-        password=request.form['psd']
-        newpsd=request.form['newpsd']
-        cpsd=request.form['cpsd']
-        if newpsd==cpsd:
-            cmd.execute("update login set password='"+cpsd+"' where id='"+str(user_id)+"' and password='"+password+"'")
-            con.commit()
-            return '''<script>alert("password changed");window.location="/cyberhome"</script>'''
-
-        else:
-            return '''<script>alert("invalid password");window.location="/change_psd"</script>'''
-
-
-@root.route('/hchange_psd')
-def hchange_psd():
-    return render_template("humanchangepass.html")
-@root.route('/hchange_psd2',methods=['get','post'])
-def hchange_psd2():
-        user_id = session['id']
-        password=request.form['psd']
-        newpsd=request.form['newpsd']
-        cpsd=request.form['cpsd']
-        if newpsd==cpsd:
-            cmd.execute("update login set password='"+cpsd+"' where id='"+str(user_id)+"' and password='"+password+"'")
-            con.commit()
-            return '''<script>alert("password changed");window.location="/hhome"</script>'''
-
-        else:
-            return '''<script>alert("invalid password");window.location="/hchange_psd"</script>'''
-
-
+    user_id = request.form['id']
+    password=request.form['psd']
+    newpsd=request.form['newpsd']
+    cpsd=request.form['cpsd']
+    cmd.execute("update login set password='"+cpsd+"' where userid='"+user_id+"' and password='"+password+"'")
+    con.commit()
+    return render_template("change password.html")
 @root.route('/cyber_report_view')
 def cyber_report_view():
     return render_template("cyber view report.html")
 @root.route('/cyber_report_view2')
 def cyber_report_view2():
-    cmd.execute("select * from admin_report where department='Cyber Cell' and status='pending'")
+    cmd.execute("select * from admin_report where department='cyber cell' and status='pending'")
     con.commit()
     d=cmd.fetchall()
     return render_template("cyber view report.html",val=d)
 @root.route('/human_report_view')
 def human_report_view():
-    cmd.execute("select * from admin_report where department='Human Right' and status='pending'")
+    cmd.execute("select * from admin_report where department='human rights' and status='pending'")
     con.commit()
     s=cmd.fetchall()
     print(s)
     return render_template("human view report.html",val=s)
 
-@root.route('/publicreg',methods=['get','post'])
-def publireg():
-    name = request.form['textfield']
-    place = request.form['textfield2']
-    email = request.form['textfield3']
-    phone=request.form['textfield4']
-    cmd.execute("insert into login values(null,'" + email + "','"+phone+"','public')")
-    id=con.insert_id()
-    cmd.execute("insert into publicregistration values('"+str(id)+"','" + name + "','"+place+"','"+email+"','"+phone+"')")
-
-    con.commit()
-    return render_template("login.html")
 
 
 
-@root.route('/publicpostdom',methods=['get','post'])
-def publicpostdom():
-    post = request.form['textarea']
-    id=session['id']
 
-    cmd.execute("insert into post values(null,'"+str(id)+"','" + post + "',curdate())")
-    con.commit()
-    return render_template("post.html")
 
-@root.route('/send',methods=['get','post'])
-def send():
-    id = request.args.get('id')
-    session['rid']=id
-    return render_template("reply.html")
 
-@root.route('/replycyber',methods=['get','post'])
-def replycyber():
-
-    id=session['rid']
-    reply=request.form['reply'];
-    cmd.execute("update admin_report set status='"+reply+"' where id='"+str(id)+"'")
-    con.commit()
-    return render_template("cybercell home.html")
-
-@root.route('/laws')
-def laws():
-    cmd.execute("select * from law ")
-    con.commit()
-    s=cmd.fetchall()
-
-    return render_template("viewlaws.html",val=s)
 
 
 
